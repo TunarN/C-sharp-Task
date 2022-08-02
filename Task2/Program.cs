a3ba21ff3fcfb3695173645b891152897dd7d12f
TunarN <tunarzn@code.edu.az>
2022-08-02 16:34:17 +0400

    Homework

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "Fevral";
+            switch (month)
+            {
+                case "Yanvar": case "Mart": case "May": case "Iyul": case "Avqust": case "Oktyabr": case "Dekabr":
+                    Console.WriteLine("31");
+                    break;
+                case "Aprel": case "Iyun": case "Sentyabr": case "Noyabr":
+                    Console.WriteLine("30");
+                    break;
+                case "Fevral":
+                    Console.WriteLine("28");
+                    break;
+
+            }
+        }
+    }
+}
